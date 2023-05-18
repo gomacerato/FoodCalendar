4394b3fa4fcb9baa938f070c2b4a89974374f931
gomacerato <119230727+gomacerato@users.noreply.github.com>
2023-05-17 22:01:45 -0500

    deez

--- controller.js
+++ controller.js
@@ -1,41 +1,44 @@
-const form = document.getElementById('addRecipeForm');
-const recipeCards = document.getElementById('recipeCards');
+const form = document.querySelector('#recipeForm');
+const inputTitle = document.querySelector('#recipeTitle');
+const inputDescrip = document.querySelector('#recipeDescription');
+const recipeList = document.querySelector('#recipeCards');
 
-form.addEventListener('submit', function(event) {
-  event.preventDefault(); // Prevent form from submitting normally
 
-  // Get the values of the form fields
-  const recipeName = document.getElementById('recipeName').value;
-  const recipeIngredients = document.getElementById('recipeIngredients').value;
-  const recipeInstructions = document.getElementById('recipeInstructions').value;
+var listStart = 7;
+function createLi(start){
+    const li = document.createElement('li');
+    li.className = "cardStyle";
+    li.id = `li${start}`;
+    const outerDiv = document.createElement('div');
+    outerDiv.className= "container";
+    const image = document.createElement('img');
+    image.className="imageSize";
+    image.src = "assets/Spaghetti-Bolognese.jpeg";
+    const title = document.createElement('h5');
+    title.className = "titleS";
+    title.textContent = inputTitle.value;
+    const description = document.createElement('p');
+    description.className = "descriptionS";
+    description.textContent = inputDescrip.value;
+    const a = document.createElement('a');
+    a.textContent = "View Recipe";
+    a.href="recipePage.html";
+    a.className="btn btn-primary";
+    a.onclick=showRecipeItem(`li${listStart}`);
+    li.appendChild(outerDiv);
+    outerDiv.appendChild(image);
+    outerDiv.appendChild(title);
+    outerDiv.appendChild(description);
+    outerDiv.appendChild(a);
 
-  // Create a new recipe card
-  const card = document.createElement('div');
-  card.classList.add('card');
+    return li;
+    
+}
 
-  const cardBody = document.createElement('div');
-  cardBody.classList.add('card-body');
-
-  const cardTitle = document.createElement('h5');
-  cardTitle.classList.add('card-title');
-  cardTitle.textContent = recipeName;
-
-  const cardIngredients = document.createElement('p');
-  cardIngredients.classList.add('card-text');
-  cardIngredients.textContent = recipeIngredients;
-
-  const cardInstructions = document.createElement('p');
-  cardInstructions.classList.add('card-text');
-  cardInstructions.textContent = recipeInstructions;
-
-  cardBody.appendChild(cardTitle);
-  cardBody.appendChild(cardIngredients);
-  cardBody.appendChild(cardInstructions);
-
-  card.appendChild(cardBody);
-
-  recipeCards.appendChild(card);
-
-  // Reset the form fields
-  form.reset();
+form.addEventListener('submit', (event)=>{
+    event.preventDefault();
+    const li = createLi(listStart);
+    recipeList.appendChild(li);
+    listStart = listStart + 1;
 });
+
---
+++ views.js
@@ -0,0 +1,23 @@
+
+
+function showRecipeItem(listNumber){
+    const listItem = document.getElementById(listNumber);
+    // Select the inner elements within the list item
+    const image = listItem.querySelector('img.imageSize');
+    const title = listItem.querySelector('h5.titleS');
+    const description = listItem.querySelector('p.descriptionS');
+    const viewRecipeLink = listItem.querySelector('a.btn-primary');
+
+    // Access the innerHTML or attributes of the selected elements
+    const imageSrc = image.src;
+    const titleText = title.innerText;
+    const descriptionText = description.innerText;
+    const viewRecipeLinkHref = viewRecipeLink.href;
+
+    // Do something with the selected elements and their values
+    // For example, update the recipe page with the retrieved details
+    document.getElementById('recipeImage').src = imageSrc;
+    document.getElementById('titleText').innerText = titleText;
+    document.getElementById('descriptionText').innerText = descriptionText;
+    document.getElementById('viewRecipeLink').href = viewRecipeLinkHref;
+}
